5baa72fb8721687876859868f6a4e03917f48e8d
Safae Elhamri <safaae.elhamri@gmail.com>
2026-09-17 22:15:44 +0100

    Add SessionAuthentication for browsable API access

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -133,6 +133,7 @@ MAILERS = {
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
